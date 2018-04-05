02bd9ccf809b1069e25aa5065da82a7a100582d8
mscott2016 <yahoomarley2@gmail.com>
2018-04-04 23:41:21 -0400

    Initial Commit

---
+++ prj2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prj2
+//
+//  Created by Marley on 4/4/18.
+//  Copyright © 2018 Mar Z Mar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
